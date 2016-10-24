0e74c5a12a96fef1970b24ee1794026155b54e9a
sophiayangx <sopyang@nuevaschool.org>
2016-10-24 08:34:20 -0700

    Classadoo autocommit

---
+++ script.js
@@ -0,0 +1,22 @@
+var name;
+var nameInput = $("#name-input")
+var noteInput = $("#note-input")
+var noteList = $("#note-list")
+
+nameInput.keydown(function(e) {
+  if (e.which == 13) {
+    // we have a new name! Now what?
+  }	
+})
+
+function getNotesForName(name) {  
+  onNewListItem(name + "/notes", function(note) {
+    console.log(note)
+  })
+}
+
+noteInput.keydown(function(e) {
+  if (e.which == 13) {    
+    addListItem(name + "/notes", noteInput.val())
+  }
+})
